ca8a9a5aa815a63ec4bcc2f54845d9b2ba88bb82
shangjieZou <31823702+shangjieZou@users.noreply.github.com>
2018-08-10 00:13:15 +0800

    Update MOOC_Homework_n_test_third_week.py

--- MOOC_Homework_n_test_third_week.py
+++ MOOC_Homework_n_test_third_week.py
@@ -1,4 +1,4 @@
-
+import math
 
 # This block is for bascketball prediction(Whether the team with higher score can win or not)
 if (0):
@@ -37,7 +37,7 @@ if(0):
     sum = 0
     extra = 0
 
-    for num in range(1, max)
+    for num in range(1, max):
 
 
         if num % 2 and not num % 3:
@@ -49,4 +49,135 @@ if(0):
     print sum
 
 
+# Homework_T1
+if(0):
+    n = int(raw_input())
+    sumNum = 0
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            sumNum += i
+    print sumNum
 
+
+# Homework T2
+if(0):
+    n = int(raw_input())
+    sumPrime = 0
+    if n == 1:
+        sumPrime = 0
+    elif n == 2:
+        sumPrime = 2
+    else:
+        for i in range(2, n):
+            for k in range(2, int(math.sqrt(i)+1)):  # This int(math.sqrt(i)+1 can save time
+                if i % k == 0:
+                    break
+            else:
+                sumPrime += i
+    print sumPrime
+
+
+# Homework T3
+if(0):
+    # 1900 is not leap year, has 365 days
+    # 1901 1.1 is Tuesday
+    def DaysInMonth(Month, year):
+        if Month==1 or Month==3 or Month==5 or Month==7 or Month==8 or Month==10 or Month==12:
+            Days_In_Month = 31
+        elif Month==4 or Month==6 or Month==9 or Month==11:
+            Days_In_Month = 30
+        else:
+            if (year % 4 ==0 and year % 100 != 0) or year % 400==0:
+                Days_In_Month = 29
+            else:
+                Days_In_Month = 28
+        return Days_In_Month
+
+    SumAllDay = 365-6
+    SumSunday = 0
+    for year in range(1901,2001):
+        for Month in range(1,13):
+            if (SumAllDay+DaysInMonth(Month,year)) % 7 == 0:
+                SumSunday += 1
+            SumAllDay += DaysInMonth(Month,year)
+    print SumSunday
+
+
+# Homework T4
+if (0):
+    # Strategy 1: Spend too long time
+    import math
+    n = int(raw_input())
+    Count = 0
+
+    def primeOrNot(number):
+        prime = str
+        if number==2:
+            prime = "Yes"
+        elif number == 3:
+            prime = "Yes"
+        elif number>3:
+            for i in range(2, number+1):
+                for k in range(2, int(math.sqrt(i) + 1)):
+                    if i % k == 0:
+                        prime = "No"
+                        break
+                else:
+                    prime = "Yes"
+        return prime
+
+
+    for i in range(2, n):
+        num_ForeheadNum = i
+        num_BackNum = 0
+        NewNum = i
+        NumForLimitTheCycle = i
+        while NumForLimitTheCycle != 0:
+            num_BackNum = NewNum % 10
+            num_ForeheadNum= NewNum / 10
+            NewNum = num_BackNum * (10 ** (len(str(i))-1)) + num_ForeheadNum
+            NumForLimitTheCycle /= 10
+            if primeOrNot(NewNum) == "No":
+                break
+        else:
+            Count += 1
+
+    print Count
+
+import math
+n = int(raw_input())
+Count = 0
+
+def primeOrNot(number):
+    prime = str
+    if number==2:
+        prime = "Yes"
+    elif number == 3:
+        prime = "Yes"
+    elif number>3:
+        for i in range(2, number+1):
+            for k in range(2, int(math.sqrt(i) + 1)):
+                if i % k == 0:
+                    prime = "No"
+                    break
+            else:
+                prime = "Yes"
+    return prime
+
+
+for i in range(2, n):
+    num_ForeheadNum = i
+    num_BackNum = 0
+    NewNum = i
+    NumForLimitTheCycle = i
+    while NumForLimitTheCycle != 0:
+        num_BackNum = NewNum % 10
+        num_ForeheadNum= NewNum / 10
+        NewNum = num_BackNum * (10 ** (len(str(i))-1)) + num_ForeheadNum
+        NumForLimitTheCycle /= 10
+        if primeOrNot(NewNum) == "No":
+            break
+    else:
+        Count += 1
+
+print Count
